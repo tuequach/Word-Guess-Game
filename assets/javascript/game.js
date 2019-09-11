0179eca2c7533e7c6e299e1243bb279919678f7b
tuequach <tuequach@gmail.com>
2019-09-11 13:57:35 -0500

    rewriting code to simplier terms with description

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,110 @@
+
+
 // defining all possible variables in global scope
-var words = ["teemo", "ahri", "graves", "annie", "alistar",
+var champions = ["teemo", "ahri", "graves", "annie", "alistar",
 "blitzcrank","ezreal", "nocturne", "katarina"];
+
+//store current word to be guessed as a string and another variable for actual letters 
+var currentWord = "";
+var currentLetters =[];
+
+//variables that hold the _ space 
+var numbers = 0;
+
+//store answer of the answers for users
+var playerChoice = [];
+//wrong gusses and display to user those that already guessed
+var incorrectLetters=[];
+
+// stats for the game
 var wins =0;
 var losses =0;
 var guesses = 15;
-var playerChoice = [];
-var letterUsed=[];
 
-var playerChoiceText = document.getElementById("playerChoice-text");
-var guessesText = document.getElementById("guesses-text");
-var letterUsedText = document.getElementById("letterUsed-text");
-var winsText = document.getElementById("wins-text");
-var lossText = document.getElementById("loss-text");
-var gameText = document.getElementById("game-text");
+//newGame function that allows randomize of the champions array
+var newGame = function() {
+    currentWord = champions[Math.floor(math.random() * champions.length)];
+        console.log(currentWord);
 
-// Start of the game and choosing random words
-var random = Math.floor(Math.random() * words.length);
-var pickingWord = words[random];
-var pickingLetters = pickingWord.split("");
+    currentLetters = currentWord.split("");
+        console.log(currentLetters);
 
-// Replaying or playing a new game
-var newGame = () => {
-    guesses = 15; 
-    letterUsed =[];
-    wordPickedLastGame = pickingWord;
-    word.splice(random, 1);
-
-    random = Math.floor(Math.random() * words.length);
-    pickingWord = word[random];
-    pickingLetters = pickingWord.split("");
-
-    words.push(wordPickedLastGame)
-
-    generateplayerChoice(); 
+    numbers = currentLetters.length;
+        console.log(numbers);
 }
 
-//generating playerchoice and by events
-var creatingplayerChoice = function () {
-    playerChoice =[];
-for (var i =0; i < pickingWord.length; i++) {
-    playerChoice.push("_");
-}
-    return playerChoice;
-}
 
-creatingplayerChoice();
+// var playerChoiceText = document.getElementById("playerChoice-text");
+// var guessesText = document.getElementById("guesses-text");
+// var letterUsedText = document.getElementById("letterUsed-text");
+// var winsText = document.getElementById("wins-text");
+// var lossText = document.getElementById("loss-text");
+// var gameText = document.getElementById("game-text");
 
-playerChoiceText.textContent = "" + playerChoice.join(" ");
-guessesText.textContent = "" +  guesses;
-letterUsedText.textContent = "" + letterUsed;
-winsText.textContent = "" + wins;
-lossText.textContent = "" + losses;
+// // Start of the game and choosing random words
+// var random = Math.floor(Math.random() * words.length);
+// var pickingWord = words[random];
+// var pickingLetters = pickingWord.split("");
 
-//function runs whenever user presses a key
-document.onkeyup = function (event) {
-    var userGuess = event.key;
-    letterUsed.push(userGuess);
+// // Replaying or playing a new game
+// var newGame = () => {
+//     guesses = 15; 
+//     letterUsed =[];
+//     wordPickedLastGame = pickingWord;
+//     word.splice(random, 1);
 
-    for (var i=0; i < pickingLetters.length; i++) {
-        if (pickingLetters[i] == userGuess.toString()) {
-            playerChoice[i] = userGuess;
-        }
-    }
-    if (pickingWord == playerChoice.join("")) {
-        gameText.textContent = "You've Won Summoner Rift by Guessing the Correct Champion.";
-        wins++; 
-        newGame();
-    } else if (guesses == 0) {
-        gameText.textContent = "Defeated by Minions, You've Lost This Round.";
-        newGame();
-    } else {
-        guesses--;
-    }
+//     random = Math.floor(Math.random() * words.length);
+//     pickingWord = word[random];
+//     pickingLetters = pickingWord.split("");
+
+//     words.push(wordPickedLastGame)
+
+//     generateplayerChoice(); 
+// }
+
+// //generating playerchoice and by events
+// var creatingplayerChoice = function () {
+//     playerChoice =[];
+// for (var i =0; i < pickingWord.length; i++) {
+//     playerChoice.push("_");
+// }
+//     return playerChoice;
+// }
+
+// creatingplayerChoice();
+
+// playerChoiceText.textContent = "" + playerChoice.join(" ");
+// guessesText.textContent = "" +  guesses;
+// letterUsedText.textContent = "" + letterUsed;
+// winsText.textContent = "" + wins;
+// lossText.textContent = "" + losses;
+
+// //function runs whenever user presses a key
+// document.onkeyup = function (event) {
+//     var userGuess = event.key;
+//     letterUsed.push(userGuess);
+
+//     for (var i=0; i < pickingLetters.length; i++) {
+//         if (pickingLetters[i] == userGuess.toString()) {
+//             playerChoice[i] = userGuess;
+//         }
+//     }
+//     if (pickingWord == playerChoice.join("")) {
+//         gameText.textContent = "You've Won Summoner Rift by Guessing the Correct Champion.";
+//         wins++; 
+//         newGame();
+//     } else if (guesses == 0) {
+//         gameText.textContent = "Defeated by Minions, You've Lost This Round.";
+//         newGame();
+//     } else {
+//         guesses--;
+//     }
  
 
-// updated screen 
-playerChoiceText.textContent = "" + playerChoice.join(" ");
-guessesText.textContent = "" +  guesses;
-letterUsedText.textContent = "" + letterUsed;
-winsText.textContent = "" + wins;
-lossText.textContent = "" + losses;
-}
+// // updated screen 
+// playerChoiceText.textContent = "" + playerChoice.join(" ");
+// guessesText.textContent = "" +  guesses;
+// letterUsedText.textContent = "" + letterUsed;
+// winsText.textContent = "" + wins;
+// lossText.textContent = "" + losses;
+// }
