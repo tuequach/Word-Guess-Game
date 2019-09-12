fffdca37ec29de96aeccd0997014a01c4d94863c
tuequach <tuequach@gmail.com>
2019-09-12 17:11:45 -0500

    adding in images for game and css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,7 @@
 
 
 // defining all possible variables in global scope
-var champions = ["teemo", "ahri", "graves", "annie", "alistar",
-"blitzcrank","ezreal", "nocturne", "katarina"];
+var champions = ["ahri", "annie", "aatrox", "azir", "blitzcrank", "draven", "fiddlesticks", "garen", "jihn", "jinx"];
 
 //store current word to be guessed as a string and another variable for actual letters 
 var currentWord = "";
@@ -11,9 +10,9 @@ var currentLetters =[];
 //variables that hold the _ space 
 var numbers = 0;
 
-//store answer of the answers for users
+//store answer of the answers for users' guesses
 var playerChoice = [];
-//wrong gusses and display to user those that already guessed
+//wrong guesses and display to user those that already guessed
 var incorrectLetters=[];
 
 // stats for the game
@@ -22,8 +21,8 @@ var losses =0;
 var guesses = 15;
 
 //newGame function that allows randomize of the champions array
-var newGame = function() {
-    currentWord = champions[Math.floor(math.random() * champions.length)];
+function newGame () {
+    currentWord = champions[Math.floor(Math.random() * champions.length)];
         console.log(currentWord);
 
     currentLetters = currentWord.split("");
@@ -31,80 +30,122 @@ var newGame = function() {
 
     numbers = currentLetters.length;
         console.log(numbers);
+
+    //stats for game in the beginning
+    incorrectLetters=[];
+    playerChoice=[];
+    guesses= 15;
+
+        if (guesses = 15) {
+            document.getElementById("ahri").removeAttribute("style");
+            document.getElementById("annie").removeAttribute("style");
+            document.getElementById("aatrox").removeAttribute("style");
+            document.getElementById("azir").removeAttribute("style");
+            document.getElementById("blitzcrank").removeAttribute("style");
+            document.getElementById("draven").removeAttribute("style");
+            document.getElementById("fiddlesticks").removeAttribute("style");
+            document.getElementById("garen").removeAttribute("style");
+            document.getElementById("jihn").removeAttribute("style");
+            document.getElementById("jinx").removeAttribute("style");
+
+        }
+    
+    for (i=0; i <numbers; i++) {
+        playerChoice.push("_");
+            console.log(playerChoice);
+    }
+
+    //changing html elements to display current information
+    document.getElementById("word").innerHTML = playerChoice.join(" ");
+    document.getElementById("guessesLeft").innerHTML = "Number of Guesses left: " + " " + guesses;
+    document.getElementById("wins").innerHTML = "Wins: " + " " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + " " + losses;
+}
+ 
+function computing(letters) {
+    //making sure keyboard input is letters only
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+    var correctLetters = false;
+
+//check if the letter is one of the letters in the words
+    for (var i=0; i< numbers; i++) {
+        if(currentWord[i] == letters) {
+            correctLetters = true;
+        }
+    }
+
+//checking where the guess letter is om the word, 
+    if (correctLetters) {
+        for (var i=0; i< numbers; i++){
+            if (currentWord[i] == letters) {
+                playerChoice[i] = letters;
+            }
+        }
+    }
+
+// if letter isn't part of the word
+    else {
+        incorrectLetters.push(letters);
+        guesses--
+    }
+    console.log(playerChoice);
+
+} 
+else {
+    alert ("Please pick a letter from the Alphabet (A-Z)");
+    }
 }
 
+var completeGame = function() {
+    //updating the html game stats
+    document.getElementById("word").innerHTML = playerChoice.join(" ");
+    document.getElementById("guessesLeft").innerHTML = "Number of Guesses left: " + " " + guesses;
+    document.getElementById("lettersPicked").innerHTML = "Letters Guessed:" + " " + incorrectLetters.join(" ");
 
-// var playerChoiceText = document.getElementById("playerChoice-text");
-// var guessesText = document.getElementById("guesses-text");
-// var letterUsedText = document.getElementById("letterUsed-text");
-// var winsText = document.getElementById("wins-text");
-// var lossText = document.getElementById("loss-text");
-// var gameText = document.getElementById("game-text");
 
-// // Start of the game and choosing random words
-// var random = Math.floor(Math.random() * words.length);
-// var pickingWord = words[random];
-// var pickingLetters = pickingWord.split("");
+    //checking if user won & lost
+    //won
+    if (currentLetters.toString() == playerChoice.toString()) {
+        wins++; 
+        alert("Yay! you've picked the right champion! '" + currentWord + "' correctly. You've won the Rift, play again?");
+        
 
-// // Replaying or playing a new game
-// var newGame = () => {
-//     guesses = 15; 
-//     letterUsed =[];
-//     wordPickedLastGame = pickingWord;
-//     word.splice(random, 1);
+        document.getElementById("wins").innerHTML = "Wins: " + " " + wins;
 
-//     random = Math.floor(Math.random() * words.length);
-//     pickingWord = word[random];
-//     pickingLetters = pickingWord.split("");
+        newGame();
+        document.getElementById("lettersPicked").innerHTML = "Letters Guessed: " + " " + " " ;
 
-//     words.push(wordPickedLastGame)
+    //lost
+    } else if (guesses == 0) { 
+        losses++;
+        alert("You've ran out of guesses and the minions attack your base. The correct champions was '" + currentWord + "'. Try Again?");
+    
+        //update stats in html
 
-//     generateplayerChoice(); 
-// }
+        document.getElementById('losses').innerHTML = "Losses: " + " " + losses;
 
-// //generating playerchoice and by events
-// var creatingplayerChoice = function () {
-//     playerChoice =[];
-// for (var i =0; i < pickingWord.length; i++) {
-//     playerChoice.push("_");
-// }
-//     return playerChoice;
-// }
+        //reseting game
+        newGame();
+        document.getElementById("lettersPicked").innerHTML = "Letters Guessed:" + " " + " ";
+    }
+}
 
-// creatingplayerChoice();
+//where the game begins
 
-// playerChoiceText.textContent = "" + playerChoice.join(" ");
-// guessesText.textContent = "" +  guesses;
-// letterUsedText.textContent = "" + letterUsed;
-// winsText.textContent = "" + wins;
-// lossText.textContent = "" + losses;
+    newGame();
 
-// //function runs whenever user presses a key
-// document.onkeyup = function (event) {
-//     var userGuess = event.key;
-//     letterUsed.push(userGuess);
+//function runs when user presses key
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        console.log(userGuess);
 
-//     for (var i=0; i < pickingLetters.length; i++) {
-//         if (pickingLetters[i] == userGuess.toString()) {
-//             playerChoice[i] = userGuess;
-//         }
-//     }
-//     if (pickingWord == playerChoice.join("")) {
-//         gameText.textContent = "You've Won Summoner Rift by Guessing the Correct Champion.";
-//         wins++; 
-//         newGame();
-//     } else if (guesses == 0) {
-//         gameText.textContent = "Defeated by Minions, You've Lost This Round.";
-//         newGame();
-//     } else {
-//         guesses--;
-//     }
- 
+    //checking letters function
+    computing(userGuess);
+    completeGame();
 
-// // updated screen 
-// playerChoiceText.textContent = "" + playerChoice.join(" ");
-// guessesText.textContent = "" +  guesses;
-// letterUsedText.textContent = "" + letterUsed;
-// winsText.textContent = "" + wins;
-// lossText.textContent = "" + losses;
-// }
+    if (guesses <= 14) {
+        document.getElementById("ahri").style.transform = "rotate (180eg)";
+        document.getElementById("ahri").style.opacity = "0.15";
+        }
+
+    }
